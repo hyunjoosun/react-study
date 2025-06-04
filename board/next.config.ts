9b0dd123768f1d4440b2f528bdf1704a3e773452
hyunjoo Seon <shj1219@idstrust.com>
2025-06-04 13:50:28 +0900

    no message

--- board/next.config.ts
+++ board/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/study/board",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
